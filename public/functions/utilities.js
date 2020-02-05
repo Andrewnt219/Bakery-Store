94a03582edf14fb2a2529a2369d6f62b2540c0ea
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-02-04 21:40:14 -0500

    revert size

--- public/functions/utilities.js
+++ public/functions/utilities.js
@@ -1,9 +1,6 @@
 function display(node) {
+    console.log('display called')
     const show = document.querySelector(`section.${node}`);
     show.classList.toggle('hidden');
     show.classList.toggle('right-block');
-}
-
-function nav(e) {
-    console.log(e);
 }
