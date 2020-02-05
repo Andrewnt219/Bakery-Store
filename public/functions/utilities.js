823a8bf3dfb9fca73497346728b69419afe6c37d
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-02-04 21:20:17 -0500

    border-radius

--- public/functions/utilities.js
+++ public/functions/utilities.js
@@ -1,6 +1,9 @@
 function display(node) {
-    console.log('display called')
     const show = document.querySelector(`section.${node}`);
     show.classList.toggle('hidden');
     show.classList.toggle('right-block');
+}
+
+function nav(e) {
+    console.log(e);
 }
