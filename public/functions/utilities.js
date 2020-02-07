1c434cea3231c69682aca16e267e074aff7da0f4
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-02-07 16:28:32 -0500

    visualize validation

--- public/functions/utilities.js
+++ public/functions/utilities.js
@@ -1,22 +1,65 @@
+/* data */
+/* !regex --> error */
+/* Add new requirement here */
+const verifiers = [
+    {
+        id: 'lowercase',
+        desc: 'At least 1 lower case',
+        regex: /[a-z]/,
+        reverse: false
+    },
+    {
+        id: 'uppercase',
+        desc: 'At least 1 upper case',
+        regex: /[A-Z]/,
+        reverse: false
+    },
+    {
+        id: 'digit',
+        desc: 'At least 1 digit',
+        regex: /\d/,
+        reverse: false
+    },
+    {
+        id: 'specialCharacter',
+        desc: 'At least 1 special character',
+        regex: /[!@#$%^&*()_+=\-,.<>?\/\[\]{};:'"]/,
+        reverse: false
+    },
+    {
+        id: 'length',
+        desc: '8 to 25 non-whitespace characters.',
+        regex: /\S{8,25}/,
+        reverse: false
+    },
+    {
+        id: 'whiteSpace',
+        desc: 'No whitespaces',
+        regex: /.*\s/,
+        reverse: true
+    },
+]
+
 /* elems */
-    /* forms */
+/* forms */
 const register = document.querySelector('#register');
 const login = document.querySelector('#login');
-    /* nav bar */
+const error = register.querySelector('.error');
+/* nav bar */
 const registerButton = document.querySelector('#register-button');
 const loginButton = document.querySelector('#login-button');
 
 /* listeners */
-registerButton.addEventListener('click',display);
-loginButton.addEventListener('click',display);
+registerButton.addEventListener('click', display);
+loginButton.addEventListener('click', display);
 register.addEventListener('submit', registerValidation);
 login.addEventListener('submit', loginValidation);
+register.password.addEventListener('change', visualizedRequirement)
 
 /* functions */
-    /* - show/hide section.:is(register/login)
-        - add active class */
+/* - show/hide section.:is(register/login)
+    - add active class */
 function display(e) {
-
     const name = String(e.target.textContent).toLowerCase().trim();
     const hidden = document.querySelector(`section.${name}`);
     if (hidden) {
@@ -25,7 +68,7 @@ function display(e) {
     this.classList.toggle('active');
 }
 
-    /* Change rating font color (threshold 4.0) */
+/* Change rating font color (threshold 4.0) */
 (function rating() {
     const p = document.querySelector('p.rating');
     if (p) {
@@ -36,50 +79,98 @@ function display(e) {
     }
 })();
 
-    /* form validation */
-function registerValidation(e) {
-    
-    const username = register.username.value.trim();
-    const password = register.password.value.trim();
-    const passwordAgain = register.passwordAgain.value.trim();
-    const phone = register.phone.value.trim() || '';
-    const email = register.email.value.trim();
+/* Add '*' to all required inputs */
+(function () {
+    const requiredInput = register.querySelectorAll('input[required]')
+    requiredInput.forEach(input => {
+        const requiredLabel = register.querySelector(`label[for=${input.name.trim()}]`);
+        console.log(requiredLabel);
+        requiredLabel.innerHTML += '<span class="required">*</span>'
+    })
+})();
 
-    const goodPassword = /(?=\S{8,16})(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=\-,.<>?\/\[\]{};:'"])/
-    const verifiers = [
-        {
-            name: 'At least 1 lower case!',
-            regex: /[a-z]/
-        },
-        {
-            name: 'At least 1 upper case!',
-            regex: /[A-Z]/
-        },
-        {
-            name: 'At least 1 digit!',
-            regex: /\d/
-        },
-        {
-            name: 'At least 1 special character!',
-            regex: /[!@#$%^&*()_+=\-,.<>?\/\[\]{};:'"]/
-        },
-        {
-            name: 'Password must be 8-25 non-whitespace characters! white spaces are ok, but not 8 whitespaces or something',
-            regex: /[\S{8,25}]/
-        },
-    ]
-    
-    if (!goodPassword.test(password)) {
-        e.preventDefault();
-        const errors = [];
-        for (veri of verifiers) {
-            if(!veri.regex.test(password))
-                errors.push(veri.name);
+/* Add password requirements */
+(function () {
+    // Create checklist into DOM
+    const checkList = document.createElement('div');
+    checkList.id = "passwordCheckList";
+    verifiers.forEach(verifier => {
+        const p = document.createElement('p');
+        p.id = verifier.id;
+        p.classList.add('checkList');
+        if (verifier.reverse) p.classList.add('check');
+        p.innerHTML = `<i class="fas fa-check"></i> ${verifier.desc}`;
+        checkList.appendChild(p);
+    })
+    const position = register.querySelector('label[for="passwordAgain"]');
+    register.insertBefore(checkList, position);
+})();
+
+/* form validation */
+function visualizedRequirement() {
+    const password = register.password.value;
+    console.log(password);
+    for (veri of verifiers) {
+        const p = register.querySelector(`#${veri.id}`);
+        switch (veri.reverse) {
+            /* Looks for invalid characters cases */
+            case false:
+                if (veri.regex.test(password)) {
+                    p.classList.add('check');
+                } else
+                    p.classList.remove('check');
+                break;
+            /* Look for required characters cases */
+            case true:
+                if (veri.regex.test(password))
+                    p.classList.remove('check');
+                else
+                    p.classList.add('check');
+                break;
+            default:
+                console.error('veri.reverse is not a boolean!');
+                break;
         }
-        console.log(errors);
     }
-    else 
+
+}
+
+// create error <p> in errorDiv
+function addError(errs, formNode) {
+    const errorDiv = formNode.querySelector('.error');
+    if (errorDiv) {
+        errorDiv.innerHTML = '';
+        errs.forEach(err => {
+            const error = document.createElement('p');
+            error.innerHTML = `<i class="fas fa-times"></i> ${err}`;
+            errorDiv.appendChild(error);
+        })
+        const submit = formNode.querySelector('input[type="submit"]');
+        formNode.insertBefore(errorDiv, submit);
+    } else 
+        console.error('error div not found!');
+
+}
+
+function registerValidation(e) {
+    // Requirements
+    // This is still need to manually added
+    const goodPassword = /(?=\S{8,16})(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=\-,.<>?\/\[\]{};:'"])(?!.*\s)/
+
+    const password = register.password.value;
+    const passwordAgain = register.passwordAgain.value;
+
+    if (goodPassword.test(password) && password === passwordAgain)
         return true;
+    else {
+        e.preventDefault();
+        let errors = [];
+        if (!goodPassword.test(password))
+            errors.push('Password\'s requirement(s) are not fulfilled!')
+        if (password !== passwordAgain)
+            errors.push("Password does not match!");
+        addError(errors,register);
+    }
 }
 
 function loginValidation(e) {
@@ -89,10 +180,11 @@ function loginValidation(e) {
     const _password = "1102"
 
     const valid = username === _username && password === _password;
-    console.log(valid);
 
-    if(!valid)
+    if (!valid) {
         e.preventDefault();
+        addError(["Username or password is incorrect!"], login);
+    }
     else
-        return true; 
+        return true;
 }   
